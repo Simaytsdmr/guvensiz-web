3ea96eb0321a74d4790f5368918cc94b33c79d59
Simay Taşdemir <simaytasdemir06@gmail.com>
2025-05-11 16:29:38 +0300

    Giriş sayfası eklendi

---
+++ giris.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Giriş Sayfası</title>
+</head>
+<body>
+    <h2>Giriş</h2>
+    <form method="post" action="kontrol.php">
+        Kullanıcı Adı: <input type="text" name="kullanici"><br>
+        Şifre: <input type="password" name="sifre"><br>
+        <input type="submit" value="Giriş Yap">
+    </form>
+</body>
+</html>
