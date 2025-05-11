f7a241150396b42aacda5c4f1fece5d1ff2e61d1
Simay Taşdemir <simaytasdemir06@gmail.com>
2025-05-11 22:07:03 +0300

    Güncel dosyalar eklendi: güvenli ve zafiyetli sürüm tamamlandı

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>Güvenli Yazılım Geliştirme Projesi</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Güvenli Yazılım Geliştirme Projesi</h1>
+    <div class="container">
+        <a href="giris_zafiyetli.php" class="button">Zafiyetli Uygulamayı Aç</a>
+        <a href="giris_guvenli.php" class="button">Güvenli Uygulamayı Aç</a>
+    </div>
+</body>
+</html>
